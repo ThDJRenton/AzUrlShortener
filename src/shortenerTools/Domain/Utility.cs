1c17296356b26a81a44c1b349860de57d3fc0038
Chris Renton <iammrrenton@gmail.com>
2020-11-19 10:08:24 +0000

    change vanity url to be unique, random and non sequential

--- src/shortenerTools/Domain/Utility.cs
+++ src/shortenerTools/Domain/Utility.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace Cloud5mins.domain
@@ -8,7 +9,7 @@ namespace Cloud5mins.domain
         //reshuffled for randomisation, same unique characters just jumbled up, use your own for security
         private const string ConversionCode = "aoq6lewdfit0nbvp3ukz8mc941gsj57r2hyx";
         private static readonly int Base = ConversionCode.Length;
-        private const int MinVanityLength = 4;
+        private const int MinVanityLength = 5;
 
         public static async Task<string> GetValidEndUrl(string vanity, StorageTableHelper stgHelper)
         {
@@ -28,6 +29,9 @@ namespace Cloud5mins.domain
         {
             if (i == 0)
                 return ConversionCode[0].ToString();
+
+            return GenerateUniqueRandomToken(i);
+
             var s = string.Empty;
             while (i > 0)
             {
@@ -49,6 +53,24 @@ namespace Cloud5mins.domain
         public static string GetShortUrl(string host, string vanity)
         {
             return host + "/" + vanity;
+        }
+
+        public static string GenerateUniqueRandomToken(int uniqueId)
+        // generates a unique, random, and alphanumeric token for the use as a url (not entirely secure but not sequential so generally not guessable)
+        {
+            const string availableChars =
+                "FjTG0s5dgWkbLf_8etOZqMzNhmp7u6lUJoXIDiQB9-wRxCKyrPcv4En3Y21aASHV";
+            using (var generator = new RNGCryptoServiceProvider())
+            {
+                var bytes = new byte[5]; //minimum size, longer the better but we want short URLs!
+                generator.GetBytes(bytes);
+                var chars = bytes
+                    .Select(b => availableChars[b % availableChars.Length]);
+                var token = new string(chars.ToArray());
+                //reshuffle the id into the token
+                var reversedToken = string.Join(string.Empty, token.Reverse());
+                return uniqueId + reversedToken;
+            }
         }
     }
 }
